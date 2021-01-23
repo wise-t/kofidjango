d326ca8bfe6023782979ee874baba62849272c1a
wise-t <lovjes4@gmail.com>
2021-01-23 16:23:28 +0000

    app for heroku edit

--- wt/settings.py
+++ wt/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fla9rqv@n4b$f)qk#z*wowt3va&&oj8&#0rjf5p#g25y0pz567'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+DEBUG = False
+ALLOWED_HOSTS = ['https://wisdsol.herokuapp.com/','127.0.0.1','localhost']
 
 
 # Application definition
