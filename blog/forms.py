b633eb10e2e01e714e9b328ee7490facef82e169
wise-t <lovjes4@gmail.com>
2021-01-26 05:11:36 +0000

    push

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import  Post,Comment,category
+from .models import  Post,Comment#,category
 #choices=[('sport','sport'),('coding','coding'),('entertaiment','entertaiment')]
-choices=category.objects.all().values_list('name','name')
+# choices=category.objects.all().values_list('name','name')
 
-choices_list=[]
-for item in choices:
-    choices_list.append(item)
+# choices_list=[]
+# for item in choices:
+#     choices_list.append(item)
 
 
 
@@ -21,7 +21,7 @@ class PostForm(forms.ModelForm):
             'author':forms.TextInput(attrs={'class':'form-control','value':'','id':'elder','type':'hidden'}),
 
             #'author':forms.Select(attrs={'class':'form-control'}),
-            'category':forms.Select(choices=choices_list,attrs={'class':'form-control'}),
+            #'category':forms.Select(choices=choices_list,attrs={'class':'form-control'}),
             'body':forms.Textarea(attrs={'class':'form-control'}),
         }
 
